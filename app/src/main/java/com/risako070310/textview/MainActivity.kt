fd48a8570e5271ade1a244c7e06bdf53fe6b32bc
risako070310 <risako070310@gmail.com>
2021-05-10 17:02:47 +0900

    create skill-textview

---
+++ app/src/main/java/com/risako070310/textview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.risako070310.textview
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "こんにちは"
+        textView.setTextColor(Color.rgb(255, 51, 153))
+
+    }
+}
